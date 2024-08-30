356c50fd204b8c6fefeced01e8496a260388110b
Ronak010325 <ronakyadav1325@gmail.com>
2024-08-30 19:55:00 +0530

    Added Animation to the Button

--- index.js
+++ index.js
@@ -1,45 +1,20 @@
+//Mouse Click or Touch
 var arr = document.querySelectorAll(".drum");
 for (var i = 0 ; i < arr.length ; i++) {
     arr[i].addEventListener("click" , function () {
         var button = this.innerHTML;
-        switch (button) {
-            case 'w':
-                var sound = new Audio("sounds/tom-1.mp3");
-                sound.play();
-            break;
-            case 'a':
-                var sound = new Audio("sounds/tom-2.mp3");
-                sound.play();
-            break;
-            case 's':
-                var sound = new Audio("sounds/tom-3.mp3");
-                sound.play();
-            break;
-            case 'd':
-                var sound = new Audio("sounds/tom-4.mp3");
-                sound.play();
-            break;
-            case 'j':
-                var sound = new Audio("sounds/snare.mp3");
-                sound.play();
-            break;
-            case 'k':
-                var sound = new Audio("sounds/crash.mp3");
-                sound.play();
-            break;
-            case 'l':
-                var sound = new Audio("sounds/kick-bass.mp3");
-                sound.play();
-            break;
-        }
+        makeSound(button);
+        buttonAnimation(button);
     });
 }
 
+//Keyboard presss
 document.addEventListener("keydown",function(event) {
-    handlekey(event.key);
+    makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
-function handlekey(key) {
+function makeSound(key) {
     console.log(key);
     switch (key) {
         case 'w':
@@ -71,4 +46,15 @@ function handlekey(key) {
             sound.play();
         break;
     }
+}
+
+//Adding .pressed class which is already defined in the CSS and removeing after some time to make an effect.
+//Using Query Selector
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    },100);
 }
